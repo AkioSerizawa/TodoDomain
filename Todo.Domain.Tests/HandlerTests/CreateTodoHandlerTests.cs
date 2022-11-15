8489ac15648d90f1a84c6322f42634fdb8ff83a4
Akio Serizawa <akio50@hotmail.com>
2022-11-15 11:16:13 -0300

    Test Handlers

--- Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
+++ Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -7,17 +7,27 @@ namespace Todo.Domain.Tests.HandlerTests;
 [TestClass]
 public class CreateTodoHandlerTests
 {
+    private readonly CreateTodoCommand _invalidCommand =
+        new CreateTodoCommand("", "", DateTime.Now);
+
+    private readonly CreateTodoCommand _validCommand =
+        new CreateTodoCommand("Titulo da Tarefa", "Akio Serizawa", DateTime.Now);
+
+    private readonly TodoHandler _handler = new TodoHandler(new FakeTodoRepository());
+
+    private GenericCommandResult _result = new GenericCommandResult();
+
     [TestMethod]
     public void Dado_um_comando_invalido_deve_interromper_a_execucao()
     {
-        var command = new CreateTodoCommand("", "", DateTime.Now);
-        var handler = new TodoHandler(new FakeTodoRepository());
-        Assert.Fail();
+        _result = (GenericCommandResult)_handler.Handle(_invalidCommand);
+        Assert.AreEqual(_result.Success, false);
     }
 
     [TestMethod]
     public void Dado_um_comando_invalido_deve_criar_a_tarefa()
     {
-        Assert.Fail();
+        _result = (GenericCommandResult)_handler.Handle(_validCommand);
+        Assert.AreEqual(_result.Success, true);
     }
 }
